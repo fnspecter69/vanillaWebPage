4a02ae33c69c24be4b1334fd071f9756657eeed9
fnspecter69 <nfreddy49@gmail.com>
2021-07-17 13:32:21 -0400

    add rest get request to page

--- js/app.js
+++ js/app.js
@@ -1,21 +1,27 @@
 document.getElementById("hello").innerHTML = "HelloWorld";
+const newDiv = document.createElement("div");
 
-var baseurl = "https://localhost:8080";
+var baseurl = "http://localhost:8080";
 var nameurl = "/name";
 
 function httpGetAsync(url) {
   var xmlHttp = new XMLHttpRequest();
-  xmlHttp.onreadystatechange = function() {
-    if (xmlHttp.readyState == 4 && xmlHttp.status == 200)
-      callback(xmlHttp.responseText);
-  }
+
+
   xmlHttp.open("GET", url, true); // true for asynchronous
-  xmlHttp.send(null);
+  //xmlHttp.setRequestHeader("Access-Control-Allow-Origin", baseurl);
   xmlHttp.onreadystatechange = function () {
     if (this.readyState == 4 && this.status == 200) {
+      console.log("success");
+      const getName = document.createTextNode(xmlHttp.responseText);
+      newDiv.appendChild(getName);
       console.log(xmlHttp.responseText);
+      document.body.insertBefore(newDiv, document.getElementById("hello"));
+    } else {
+      console.log("error");
     }
   }
+  xmlHttp.send(null);
   console.log("end ajax call");
 }
 
