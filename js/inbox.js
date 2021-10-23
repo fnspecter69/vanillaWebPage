7a6f2641000cecef5a0d0d0ce60148055d7e94b5
fnspecter69 <nfreddy49@gmail.com>
2021-10-23 14:45:10 -0400

    parse email

--- js/inbox.js
+++ js/inbox.js
@@ -1,8 +1,23 @@
 var baseurl = "http://localhost:8080";
-var getAllEmails = "/emails";
+var getAllEmailsUrl = "/emails";
 
 function displayAllEmail() {
   console.log("displayAllEmail");
+  var url = baseurl + getAllEmailsUrl;
+  var xmlHttp = new XMLHttpRequest();
+
+  xmlHttp.open("GET", url, true);
+  xmlHttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 302) {
+      console.log("success get all emails");
+      const response = JSON.parse(xmlHttp.responseText);
+      response.forEach((email, i) => {
+        console.log(email);
+      });
+
+    }
+  }
+  xmlHttp.send(null);
 }
 window.onload = function () {
   displayAllEmail();
