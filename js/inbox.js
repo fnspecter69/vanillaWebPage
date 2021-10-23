de2f5200c756cff5ee192de0f6a9c93cc9f8c1a1
fnspecter69 <nfreddy49@gmail.com>
2021-10-23 13:06:56 -0400

    add inbox page

---
+++ js/inbox.js
@@ -0,0 +1,9 @@
+var baseurl = "http://localhost:8080";
+var getAllEmails = "/emails";
+
+function displayAllEmail() {
+  console.log("displayAllEmail");
+}
+window.onload = function () {
+  displayAllEmail();
+}
