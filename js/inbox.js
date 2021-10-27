a2ab27bd14ab448faee7b5fbf8573509de270e06
fnspecter69 <nfreddy49@gmail.com>
2021-10-26 22:26:10 -0400

    display sender name

--- js/inbox.js
+++ js/inbox.js
@@ -8,12 +8,23 @@ function displayAllEmail() {
 
   xmlHttp.open("GET", url, true);
   xmlHttp.onreadystatechange = function() {
+    var emailDiv = document.createElement("div");
+    var list = document.createElement('ul');
     if (this.readyState == 4 && this.status == 302) {
       console.log("success get all emails");
       const response = JSON.parse(xmlHttp.responseText);
       response.forEach((email, i) => {
+        var liElem = document.createElement("li");
         console.log(email);
+        console.log(email.from);
+        liElem.textContent = email.from;
+        console.log(liElem);
+        list.appendChild(liElem);
       });
+      console.log(list);
+      var inbox = document.querySelector('#inboxContent');
+      inbox.appendChild(list);
+      //document.body.insertAfter(inbox, document.getElementById("inboxContent"));
 
     }
   }
