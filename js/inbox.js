1b5bde99720bed301ea40eba452ed188a8671a15
fnspecter69 <nfreddy49@gmail.com>
2021-11-09 06:50:25 -0500

    add checkbox to email and delete button

--- js/inbox.js
+++ js/inbox.js
@@ -1,5 +1,7 @@
 var baseurl = "http://localhost:8080";
 var getAllEmailsUrl = "/emails";
+var deleteButton = document.createElement('button');
+deleteButton.innerHTML = "delete";
 
 function displayAllEmail() {
   console.log("displayAllEmail");
@@ -10,25 +12,34 @@ function displayAllEmail() {
   xmlHttp.onreadystatechange = function() {
     var emailDiv = document.createElement("div");
     var list = document.createElement('ul');
+
     if (this.readyState == 4 && this.status == 302) {
       console.log("success get all emails");
       const response = JSON.parse(xmlHttp.responseText);
       response.forEach((email, i) => {
         var liElem = document.createElement("li");
+        var checkboxElem = document.createElement("input");
+        checkboxElem.setAttribute("type", "checkbox");
+        checkboxElem.setAttribute("for", email.id);
+        var emailLabel = document.createElement("label");
+        emailLabel.textContent = email.from;
         console.log(email);
-        console.log(email.from);
-        liElem.textContent = email.from;
-        console.log(liElem);
+        console.log(emailLabel);
+        liElem.appendChild(checkboxElem);
+        liElem.appendChild(emailLabel);
         list.appendChild(liElem);
       });
       console.log(list);
       var inbox = document.querySelector('#inboxContent');
       inbox.appendChild(list);
-      //document.body.insertAfter(inbox, document.getElementById("inboxContent"));
 
     }
   }
   xmlHttp.send(null);
+}
+
+function deleteEmail(){
+  console.log("delete click");
 }
 window.onload = function () {
   displayAllEmail();
